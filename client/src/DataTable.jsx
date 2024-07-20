5713f31b8dc1878ad9ac5ecf41508e30c0392f4c
BhavyaG03 <gulatibhavya30@gmail.com>
2024-07-20 23:57:38 +0530

    sorting added

--- client/src/DataTable.jsx
+++ client/src/DataTable.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { useTable } from 'react-table';
+import { useTable, useSortBy } from 'react-table';
+import { FaSortDown } from "react-icons/fa6";
+import { FaSortUp } from "react-icons/fa";
+import { FaSort } from "react-icons/fa6";
 
 const DataTable = ({ data, columns }) => {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
-    columns,
-    data,
-  });
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
 
   return (
     <table {...getTableProps()} className="min-w-full mt-5 bg-[#17181E]">
@@ -13,8 +21,20 @@ const DataTable = ({ data, columns }) => {
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()} className="py-3 px-4 text-left font-medium text-white text-[14px] montserrat-font ">
-                {column.render('Header')}
+              <th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                className="py-3 px-4 text-left font-medium text-white text-[14px] montserrat-font cursor-pointer"
+              >
+                <div className="flex items-center">
+                  {column.render('Header')}
+                  <span className="ml-2">
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? <FaSortDown size={20} className='text-white' />
+                        : <FaSortUp size={20} className='text-white' />
+                      : <FaSort size={20} className='text-white' />}
+                  </span>
+                </div>
               </th>
             ))}
           </tr>
@@ -26,7 +46,7 @@ const DataTable = ({ data, columns }) => {
           return (
             <tr {...row.getRowProps()} className="bg-[#1F2A40]">
               {row.cells.map(cell => (
-                <td {...cell.getCellProps()} className="py-3 px-4 font-light text-white text-[14px] font-sans ">
+                <td {...cell.getCellProps()} className="py-3 px-4 font-light text-white text-[14px] font-sans">
                   {cell.render('Cell')}
                 </td>
               ))}
